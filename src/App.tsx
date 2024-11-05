a05008d3a10fa37e30b161ae429f160c0f6d4b74
Bruce Röttgers <bruce@roettgers.co>
2024-11-05 12:25:40 +0100

    add minimal interactivity to the example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { Box, Label, Renderer, Window } from "@reactgjs/renderer";
-import "react-gnome"
+import React, { useState } from "react";
+import {
+  Align,
+  Box,
+  Button,
+  Label,
+  Renderer,
+  Window,
+} from "@reactgjs/renderer";
+import "react-gnome";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <Window quitOnClose minWidth={200} minHeight={200}>
-      <Box>
-        <Label>Hello, World</Label>
+    <Window quitOnClose minWidth={800} minHeight={600}>
+      <Box horizontalAlign={Align.CENTER} verticalAlign={Align.CENTER}>
+        <Label>Count: {count.toString()}</Label>
+        <Button onClick={() => setCount((count) => count + 1)}>++</Button>
       </Box>
     </Window>
   );
